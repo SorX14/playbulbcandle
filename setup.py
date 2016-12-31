16686cc3427dacf24d711fa89f8808c489678dd9
Steve Parker <steve.parker43@gmail.com>
2016-12-31 09:53:07 +0000

    Renaming and preparing to package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'playbulb-candle',
+  packages = ['playbulb-candle'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python library for communicating with PlayBulb Candles.',
+  author = 'Steve Parker',
+  author_email = 'steve.parker43@gmail.com',
+  url = 'https://github.com/SorX14/playbulb-candle', 
+  download_url = 'https://github.com/SorX14/playbulb-candle/tarball/0.1', 
+  keywords = ['playbulb'], 
+  classifiers = [],
+)
